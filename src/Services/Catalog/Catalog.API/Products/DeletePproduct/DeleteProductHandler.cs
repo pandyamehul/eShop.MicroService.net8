8d211e0962d968720469bb0f2f06b92145fbd145
pandyamehul <pandya.mehul@live.com>
2025-01-30 12:51:30 +0100

    20250130#75-80: implemented update and delete operation, document, rest client update

---
+++ src/Services/Catalog/Catalog.API/Products/DeletePproduct/DeleteProductHandler.cs
@@ -0,0 +1,19 @@
+﻿using Catalog.API.Products.GetProduct;
+
+namespace Catalog.API.Products.DeleteProduct;
+
+public record DeleteProductCommand(Guid Id) : ICommand<DeleteProductResult>;
+public record DeleteProductResult(bool IsSuccess);
+
+internal class DeleteProductCommandHandler
+    (IDocumentSession session, ILogger<GetProductsQueryHandler> logger)
+    : ICommandHandler<DeleteProductCommand, DeleteProductResult>
+{
+    public async Task<DeleteProductResult> Handle(DeleteProductCommand command, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("DeleteProductCommandHandler.Handle : called with {@Query}", command);
+        session.Delete<Product>(command.Id);
+        await session.SaveChangesAsync(cancellationToken);
+        return new DeleteProductResult(true);
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
@@ -0,0 +1,41 @@
+﻿
+using Catalog.API.Products.GetProduct;
+
+namespace Catalog.API.Products.UpdateProduct;
+
+public record UpdateProductCommand(
+        Guid Id,
+        string Name,
+        string Description,
+        string ImageFile,
+        decimal Price,
+        List<string> Category
+    )
+    : ICommand<UpdateProductResult>;
+
+public record UpdateProductResult(bool IsSuccess);
+
+internal class UpdateProductCommandHandler
+    (IDocumentSession session, ILogger<GetProductsQueryHandler> logger)
+    : ICommandHandler<UpdateProductCommand, UpdateProductResult>
+{
+    public async Task<UpdateProductResult> Handle(UpdateProductCommand command, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("UpdateProductCommandHandler.Handle : called with {@Query}", command);
+        var product = await session.LoadAsync<Product>(command.Id, cancellationToken);
+        if (product == null)
+        {
+            throw new ProductNotFoundException();
+        }
+        product.Name = command.Name;
+        product.Description = command.Description;
+        product.ImageFile = command.ImageFile;
+        product.Price = command.Price;
+        product.Category = command.Category;
+
+        session.Update(product);
+        await session.SaveChangesAsync(cancellationToken);
+
+        return new UpdateProductResult(true);
+    }
+}
