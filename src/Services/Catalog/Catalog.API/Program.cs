2f0b718a14dc7bc10166ed3bea6c3f4ae49c7478
pandyamehul <pandya.mehul@live.com>
2025-02-17 17:17:43 +0100

    20250217#91 - Register custom exception in application pipeline

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,7 +1,3 @@
-using BuildingBlocks.Behaviours;
-using Microsoft.AspNetCore.Diagnostics;
-using Microsoft.AspNetCore.Mvc;
-
 var builder = WebApplication.CreateBuilder(args);
 
 var assembly = typeof(Program).Assembly;
@@ -16,41 +12,52 @@ builder.Services.AddValidatorsFromAssembly(assembly);
 //Add Services to container
 builder.Services.AddCarter();
 
-
+//Add Marten library support for DB Crud operations
 builder.Services.AddMarten( options =>
 {
     options.Connection(builder.Configuration.GetConnectionString("Database")!);
 }).UseLightweightSessions();
 
+//Add Custom and Generic Exception handler
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+
+// Initiate and Run application pipeline
 var app = builder.Build();
 
 //Configure HTTP request pipeline
 app.MapCarter();
 
+#region 20250217: Start - Commented below block to replace with generic implementation
+// 20250217: Start - Commented below block to replace with generic implementation
 //ASP.net core global exception handling
-app.UseExceptionHandler( exceptionHandlerApp =>
-{
-    exceptionHandlerApp.Run(async context =>
-    {
-        var exception = context.Features.Get<IExceptionHandlerFeature>()?.Error;
-        if(exception == null)
-        {
-            return;
-        }
-        var exceptionDetails = new ProblemDetails
-        {
-            Title = exception.Message,
-            Status = StatusCodes.Status500InternalServerError,
-            Detail = exception.StackTrace
-        };
-        var logger = context.RequestServices.GetRequiredService<ILogger<Program>>();
-        logger.LogError(exception, exception.Message);
+//app.UseExceptionHandler( exceptionHandlerApp =>
+//{
+//    exceptionHandlerApp.Run(async context =>
+//    {
+//        var exception = context.Features.Get<IExceptionHandlerFeature>()?.Error;
+//        if(exception == null)
+//        {
+//            return;
+//        }
+//        var exceptionDetails = new ProblemDetails
+//        {
+//            Title = exception.Message,
+//            Status = StatusCodes.Status500InternalServerError,
+//            Detail = exception.StackTrace
+//        };
+//        var logger = context.RequestServices.GetRequiredService<ILogger<Program>>();
+//        logger.LogError(exception, exception.Message);
 
-        context.Response.StatusCode = StatusCodes.Status500InternalServerError;
-        context.Response.ContentType = "application/problem+json";
-        
-        await context.Response.WriteAsJsonAsync(exceptionDetails);
-    });
-});
+//        context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+//        context.Response.ContentType = "application/problem+json";
+
+//        await context.Response.WriteAsJsonAsync(exceptionDetails);
+//    });
+//});
+// 20250217: End - Commented below block to replace with generic implementation
+#endregion 20250217: End - Commented below block to replace with generic implementation
+
+//Configure app to use custom exception handling, empty { } indicates pipeline is 
+app.UseExceptionHandler(optios => { });
 
 app.Run();
