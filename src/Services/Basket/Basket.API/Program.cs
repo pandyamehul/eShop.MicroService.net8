c07925e5f639617ae488336ffa8271a3418f98a1
pandyamehul <pandya.mehul@live.com>
2025-02-22 12:04:04 +0100

    20250222#S-8, 102-103: Basket Microservices with Vertical Slice Architecture and CQRS intro & project setup

---
+++ src/Services/Basket/Basket.API/Program.cs
@@ -0,0 +1,13 @@
+//Before building application
+var builder = WebApplication.CreateBuilder(args);
+
+//Add services to container - dependency injection
+
+//Configure http request pipeline
+
+
+// After building application 
+var app = builder.Build();
+
+//Run application
+app.Run();
