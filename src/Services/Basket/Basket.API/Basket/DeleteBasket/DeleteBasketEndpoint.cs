521b2608a90a753089027c8079e19a4a4ec50717
pandyamehul <pandya.mehul@live.com>
2025-02-23 11:42:35 +0100

    20250223#113-114:Develop DeleteBasket Handler & Endpoint

--- src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketEndpoint.cs
+++ src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketEndpoint.cs
@@ -1,6 +1,24 @@
-﻿namespace Basket.API.Basket.DeleteBasket
+﻿namespace Basket.API.Basket.DeleteBasket;
+
+//public record DeleteBasketRequest(string UserName);
+public record DeleteBasketResponse(bool ISSuccess);
+
+public class DeleteBasketEndpoint : ICarterModule
 {
-    public class DeleteBasketEndpoint
+    public void AddRoutes(IEndpointRouteBuilder app)
     {
+        app.MapDelete(
+            "/basket/{userName}",
+            async (string userName, ISender sender) =>
+            {
+                var result = await sender.Send(new DeleteBasketCommand(userName));
+                var response = result.Adapt<DeleteBasketResponse>();
+                return Results.Ok(response);
+            }
+        ).WithName("DeleteBasket")
+        .Produces<DeleteBasketResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("DeleteBasketForUserName")
+        .WithDescription("Delete Basket for user");
     }
 }
---
+++ src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketHandler.cs
@@ -0,0 +1,23 @@
+﻿
+namespace Basket.API.Basket.DeleteBasket;
+
+public record DeleteBasketCommand(string UserName):ICommand<DeleteBasketResult>;
+public record DeleteBasketResult(bool ISSuccess);
+
+public class DeleteBasketCommandValidator :
+    AbstractValidator<DeleteBasketCommand>
+{
+    public DeleteBasketCommandValidator()
+    {
+        RuleFor(basket => basket.UserName).NotNull().WithMessage("Cart cannot be null");
+    }
+}
+
+public class DeleteBasketHandler
+    : ICommandHandler<DeleteBasketCommand, DeleteBasketResult>
+{
+    public async Task<DeleteBasketResult> Handle(DeleteBasketCommand request, CancellationToken cancellationToken)
+    {
+        return new DeleteBasketResult(true);
+    }
+}
