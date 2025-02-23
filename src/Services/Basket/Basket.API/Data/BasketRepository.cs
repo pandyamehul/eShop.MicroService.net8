2a1aa8da901fac18cfb480c524e812a840f1f7ee
pandyamehul <pandya.mehul@live.com>
2025-02-23 20:33:40 +0100

    20250223#116-119: Test basker API, Understand infra data concern, IBasket interface & repository implementation

---
+++ src/Services/Basket/Basket.API/Data/BasketRepository.cs
@@ -0,0 +1,19 @@
+﻿namespace Basket.API.Data;
+
+public class BasketRepository : IBasketRepository
+{
+    public Task<ShoppingCart> DeleteBasket(string userName, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ShoppingCart> GetBasket(string userName, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ShoppingCart> StoreBasket(ShoppingCart basket, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+}
