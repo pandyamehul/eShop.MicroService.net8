ab3289b31cabf15e44fab757e020c215e91138e1
pandyamehul <pandya.mehul@live.com>
2025-02-23 08:44:36 +0100

    20250223#111: Develop Store Basket Endpoint with Minimal Apis and Carter

--- src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
@@ -2,7 +2,7 @@
 namespace Basket.API.Basket.StoreBasket;
 
 public record StoreBasketCommand(ShoppingCart Cart) : ICommand<StoreBasketResult>;
-public record StoreBasketResult(string userName);
+public record StoreBasketResult(string UserName);
 
 public class StoreBasketCommandValidator : AbstractValidator<StoreBasketCommand>
 {
