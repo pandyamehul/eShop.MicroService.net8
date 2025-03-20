a68877e35a4df625fbe08482d54ce394f729e71c
pandyamehul <pandya.mehul@live.com>
2025-03-20 11:46:04 +0100

    20250320#142-157: gRPC intro and discount service implementation

---
+++ src/Services/Discount/Discount.gRPC/Data/DiscountContext.cs
@@ -0,0 +1,15 @@
+﻿using Discount.gRPC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Discount.gRPC.Data;
+
+public class DiscountContext : DbContext
+{
+    public DbSet<Coupon> Coupones { get; set; } = default!;
+
+    public DiscountContext(DbContextOptions<DiscountContext> options) 
+        : base(options)
+    {
+
+    }
+}
