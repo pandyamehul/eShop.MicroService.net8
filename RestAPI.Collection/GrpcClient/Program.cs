34f4fb860e57f8650d8ff9fa56cc46a58ed687a7
pandyamehul <pandya.mehul@live.com>
2025-06-08 18:15:29 +0200

    20250608#160-166: Discount Grpc service implementation - CRUD operations

---
+++ RestAPI.Collection/GrpcClient/Program.cs
@@ -0,0 +1,47 @@
+﻿using Grpc.Net.Client;
+using Discount.Grpc;
+
+// create a new gRPC channel to connect to the server
+using var channel = GrpcChannel.ForAddress("https://localhost:5052");
+// create a new gRPC client for the DiscountProtoService
+var client = new DiscountProtoService.DiscountProtoServiceClient(channel);
+
+// -- GET DISCOUNT --
+// create a request and call the GetDiscountAsync method
+var request = new GetDiscountRequest { ProductName = "IPhone X" };
+// Calling the GetDiscountAsync method to get product discount information
+var response = await client.GetDiscountAsync(request);
+Console.WriteLine("\nGrpc Message Response: " + response);
+Console.WriteLine($"\nProduct: {response.ProductName}, \nDescription: {response.Description}, \nAmount: {response.Amount}\n");
+
+// --CREATE DISCOUNT--
+var createDiscount = new CreateDiscountRequest
+{
+    Coupon = new CouponModel
+    {
+        ProductName = "Nokia 3310",
+        Description = "Launch discount",
+        Amount = 75
+    }
+};
+var createdDiscountResponse = await client.CreateDiscountAsync(createDiscount);
+Console.WriteLine("\nGrpc Message Response: " + createdDiscountResponse + ". \n");
+
+// -- UPDATE DISCOUNT --
+var updateDiscount = new UpdateDiscountRequest
+{
+    Coupon = new CouponModel
+    {
+        Id = 4,
+        ProductName = "IPhone 16 Pro",
+        Description = "Launch discount updated",
+        Amount = 190
+    }
+};
+var updatedDiscountResponse = await client.UpdateDiscountAsync(updateDiscount);
+Console.WriteLine("\nGrpc Message Response: " + updatedDiscountResponse + ". \n");
+
+// -- DELETE DISCOUNT --
+var deleteDiscount = new DeleteDiscountRequest { ProductName = "Nokia 3310" };
+var deletedDiscountResponse = await client.DeleteDiscountAsync(deleteDiscount);
+Console.WriteLine("\nGrpc Message Response: " + deletedDiscountResponse + ". \n");
---
+++ src/Services/Discount/Discount.gRPC/Services/DiscountService.cs
@@ -0,0 +1,92 @@
+﻿using Discount.gRPC.Data;
+using Discount.gRPC.Models;
+using Discount.Grpc;
+using Grpc.Core;
+using Mapster;
+using Microsoft.EntityFrameworkCore;
+
+namespace Discount.gRPC.Services;
+
+public class DiscountService
+    (DiscountContext discountDbContext, ILogger<DiscountService> logger)
+    : DiscountProtoService.DiscountProtoServiceBase
+{
+    public override async Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+    {
+        var coupon = await discountDbContext
+            .Coupones
+            .FirstOrDefaultAsync(c => 
+                c.ProductName == request.ProductName
+            );
+        if (coupon == null)
+        {
+            coupon = new Coupon { ProductName = "No Product", Amount = 0, Description = "No Discount"};
+        }
+        logger.LogInformation("Discount is retrieved for Product: {productName}, Amount : {amount} ", coupon.ProductName, coupon.Amount);
+        var couponModel = coupon.Adapt<CouponModel>();
+        return couponModel;
+    }
+
+    public override async Task<CouponModel> CreateDiscount(CreateDiscountRequest request, ServerCallContext context)
+    {
+        var coupon=request.Coupon.Adapt<Coupon>();
+        if (coupon is null)
+        {
+            throw new RpcException(new Status(StatusCode.InvalidArgument, "Invalid Request Parameter : "));
+
+        }
+
+        discountDbContext.Coupones.Add(coupon);
+        await discountDbContext.SaveChangesAsync();
+
+        logger.LogInformation(
+            "Discount is successfully created. Product: {productName}, Amount : {amount}"
+            , coupon.ProductName
+            , coupon.Amount
+        );
+        var couponModel = coupon.Adapt<CouponModel>();
+        return couponModel;
+    }
+
+    public override async Task<CouponModel> UpdateDiscount(UpdateDiscountRequest request, ServerCallContext context)
+    {
+        var coupon = request.Coupon.Adapt<Coupon>();
+        if (coupon is null)
+        {
+            throw new RpcException(new Status(StatusCode.InvalidArgument, "Invalid Request Parameter : "));
+
+        }
+
+        discountDbContext.Coupones.Update(coupon);
+        await discountDbContext.SaveChangesAsync();
+
+        logger.LogInformation(
+            "Discount is successfully updated. Product: {productName}, Amount : {amount}"
+            , coupon.ProductName
+            , coupon.Amount
+        );
+        var couponModel = coupon.Adapt<CouponModel>();
+        return couponModel;
+    }
+
+    public override async Task<DeleteDiscountResponse> DeleteDiscount(DeleteDiscountRequest request, ServerCallContext context)
+    {
+        var coupon = await discountDbContext
+            .Coupones
+            .FirstOrDefaultAsync(c =>
+                c.ProductName == request.ProductName
+            );
+        if (coupon == null)
+        {
+            coupon = new Coupon { ProductName = "No Product", Amount = 0, Description = "No Discount" };
+        }
+        discountDbContext.Coupones.Remove(coupon);
+        await discountDbContext.SaveChangesAsync();
+
+        logger.LogInformation(
+            "Discount is successfully Deleted. Product: {productName}"
+            , coupon.ProductName
+        );
+        return new DeleteDiscountResponse { Success = true };
+    }
+}
