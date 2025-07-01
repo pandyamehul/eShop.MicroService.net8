77f15470cb0a96f27b35282ba68ab617753640a0
pandyamehul <pandya.mehul@live.com>
2025-07-01 22:20:02 +0200

    20250701#234:Develop Orders CreateOrderCommand and Handler in Ordering.Application Layer

---
+++ src/Services/Order/Order.Application/Orders/Commands/CreateOrder/CreateOrderHandler.cs
@@ -0,0 +1,15 @@
+﻿using BuildingBlocks.CQRS;
+
+namespace Order.Application.Orders.Commands.CreateOrder;
+
+public class CreateOrderHandler
+    : ICommandHandler<CreateOrderCommand, CreateOrderResult>
+{
+    public Task<CreateOrderResult> Handle(CreateOrderCommand request, CancellationToken cancellationToken)
+    {
+        // Create order entity from command object
+        // save entity to database
+        // return the result
+        throw new NotImplementedException();
+    }
+}
