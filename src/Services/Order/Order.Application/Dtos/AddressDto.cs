4839643c574bf03449457a2275789628f201164e
pandyamehul <pandya.mehul@live.com>
2025-07-01 17:35:09 +0200

    20250701#225-233: Pattern overview CQRS, Event Source and Project skeleton setup

---
+++ src/Services/Order/Order.Application/Dtos/AddressDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Order.Application.Dtos;
+
+public record AddressDto(
+    string Name,
+    string LastName,
+    string EmailAddress,
+    string AddressLine,
+    string Country,
+    string State,
+    string City,
+    string ZipCode
+);
---
+++ src/Services/Order/Order.Application/Dtos/OrderDto.cs
@@ -0,0 +1,14 @@
+﻿using Order.Domain.Enums;
+
+namespace Order.Application.Dtos;
+
+public record OrderDto(
+    Guid Id,
+    Guid CustomerId,
+    string OrderName,
+    AddressDto ShippingAddress,
+    AddressDto BillingAddress,
+    PaymentDto Payment,
+    OrderStatus OrderStatus,
+    List<OrderItemDto> OrderItems
+);
