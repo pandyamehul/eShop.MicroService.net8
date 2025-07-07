fe1496f1b3bf385d25dfeb40ea41df9d4c7af919
pandyamehul <pandya.mehul@live.com>
2025-07-08 00:50:13 +0200

    20250708#276: Develop BasketCheckout Event in BuildingBlocks.Messaging Shared Class Library

---
+++ src/BuildingBlocks/BuildingBlocks.Messaging/Events/IntegrationEvent.cs
@@ -0,0 +1,9 @@
+﻿using System.Diagnostics;
+
+namespace BuildingBlocks.Messaging.Events;
+
+public record IntegrationEvent {
+    public Guid Id => Guid.NewGuid();
+    public DateTime EventTime => DateTime.UtcNow;
+    public string EventType => GetType().AssemblyQualifiedName;
+}
