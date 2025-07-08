6a8bdc71a20703fd72921f9e40f800afe8ebec09
pandyamehul <pandya.mehul@live.com>
2025-07-08 15:05:06 +0200

    20250708#Develop BasketCheckout Feature endpoint and publish message

---
+++ src/Services/Basket/Basket.API/Basket/CheckoutBasket/CheckOutBasketEndpoint.cs
@@ -0,0 +1,24 @@
+﻿namespace Basket.API.Basket.CheckoutBasket;
+
+public record CheckOutBasketRequest(BasketCheckOutDto BasketCheckOutDto);
+
+public record CheckOutBasketResponse(bool IsSuccess);
+
+public class CheckOutBasketEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPost("/basket/checkout", async (CheckOutBasketRequest request, ISender sender) =>
+        {
+            var command = request.Adapt<CheckOutBasketCommand>();
+            var result = await sender.Send(command);
+            var response = result.Adapt<CheckOutBasketResponse>();
+            return Results.Ok(response);
+        })
+        .WithName("CheckoutBasket")
+        .Produces<CheckOutBasketResponse>(StatusCodes.Status201Created)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Checkout Basket")
+        .WithDescription("Checkout Basket");
+    }
+}
---
+++ src/Services/Basket/Basket.API/Basket/CheckoutBasket/CheckOutBasketHandler.cs
@@ -0,0 +1,52 @@
+﻿using BuildingBlocks.Messaging.Events;
+using MassTransit;
+
+namespace Basket.API.Basket.CheckoutBasket;
+
+public record CheckOutBasketCommand(BasketCheckOutDto BasketCheckOutDto)
+    : ICommand<CheckOutBasketResult>;
+public record CheckOutBasketResult(bool IsSuccess);
+
+public class CheckOutBasketCommandValidator : AbstractValidator<CheckOutBasketCommand>
+{
+    public CheckOutBasketCommandValidator()
+    {
+        RuleFor(x => x.BasketCheckOutDto).NotNull().WithMessage("Basket Checkout Dto can't be null");
+        RuleFor(x => x.BasketCheckOutDto.UserName).NotEmpty().WithMessage("User Name is required");
+    }
+}
+
+public class CheckoutBasketCommandHandler
+    (IBasketRepository repository, IPublishEndpoint publishEndpoint)
+    : ICommandHandler<CheckOutBasketCommand, CheckOutBasketResult>
+{
+    public async Task<CheckOutBasketResult> Handle(CheckOutBasketCommand command, CancellationToken cancellationToken)
+    {
+        // get existing basket with total price
+        // Set totalprice on basketcheckout event message
+        // send basket checkout event to rabbitmq using masstransit
+        // delete the basket
+
+        // Get basket details for user from db
+        var basket = await repository.GetBasket(command.BasketCheckOutDto.UserName, cancellationToken);
+        
+        // return false if basket for user is blank
+        if (basket == null)
+        {
+            return new CheckOutBasketResult(false);
+        }
+        
+        // prepare event message
+        var eventMessage = command.BasketCheckOutDto.Adapt<BasketCheckoutEvent>();
+        eventMessage.TotalPrice = basket.TotalPrice;
+
+        // publish event message
+        await publishEndpoint.Publish(eventMessage, cancellationToken);
+
+        //delete basket after publish event
+        await repository.DeleteBasket(command.BasketCheckOutDto.UserName, cancellationToken);
+
+        // return checkout result
+        return new CheckOutBasketResult(true);
+    }
+}
