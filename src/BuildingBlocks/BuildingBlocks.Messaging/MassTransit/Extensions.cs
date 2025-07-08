644953b784e6d32bc95d4ba99e6e772fdbfff5c1
pandyamehul <pandya.mehul@live.com>
2025-07-08 11:22:02 +0200

    20250708#278: Develop MassTransit Extention Methods to Register RabbitMQ connection into DI

--- src/BuildingBlocks/BuildingBlocks.Messaging/MassTransit/Extensions.cs
+++ src/BuildingBlocks/BuildingBlocks.Messaging/MassTransit/Extensions.cs
@@ -9,27 +9,31 @@ public static class Extentions
     public static IServiceCollection AddMessageBroker(
         this IServiceCollection services, 
         IConfiguration configuration, 
+        // null is for publisher
         Assembly? assembly = null
     )
     {
         // Implement RabbitMQ - MassTransit Configurations
-        //services.AddMassTransit(config =>
-        //{
-        //    config.SetKebabCaseEndpointNameFormatter();
+        services.AddMassTransit(config =>
+        {
+            // set name convension for configration
+            config.SetKebabCaseEndpointNameFormatter();
 
-        //    if (assembly != null)
-        //        config.AddConsumers(assembly);
+            // Check for consumers to register automatically and consumer messages
+            if (assembly != null)
+                config.AddConsumers(assembly);
 
-        //    config.UsingRabbitMq((context, configurator) =>
-        //    {
-        //        configurator.Host(new Uri(configuration["MessageBroker:Host"]!), host =>
-        //        {
-        //            host.Username(configuration["MessageBroker:UserName"]);
-        //            host.Password(configuration["MessageBroker:Password"]);
-        //        });
-        //        configurator.ConfigureEndpoints(context);
-        //    });
-        //});
+            // Configure RabbitMQ connection
+            config.UsingRabbitMq((context, configurator) =>
+            {
+                configurator.Host(new Uri(configuration["MessageBroker:Host"]!), host =>
+                {
+                    host.Username(configuration["MessageBroker:UserName"]!);
+                    host.Password(configuration["MessageBroker:Password"]!);
+                });
+                configurator.ConfigureEndpoints(context);
+            });
+        });
 
         return services;
     }
