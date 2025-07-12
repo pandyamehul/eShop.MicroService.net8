6b4b3f484d013eb23768facbaa763fca772f4fcb
pandyamehul <pandya.mehul@live.com>
2025-07-12 22:24:00 +0200

    20250712#318-325: Create eshop web app and base proect structures and models

---
+++ src/WebApps/eShop.Web/Models/Order/OrderModel.cs
@@ -0,0 +1,46 @@
+﻿namespace eShop.Web.Models.Order;
+
+public record OrderModel(
+    Guid Id,
+    Guid CustomerId,
+    string OrderName,
+    AddressModel ShippingAddress,
+    AddressModel BillingAddress,
+    PaymentModel Payment,
+    OrderStatus Status,
+    List<OrderItemModel> OrderItems
+);
+
+public record OrderItemModel(Guid OrderId, Guid ProductId, int Quantity, decimal Price);
+
+public record AddressModel(
+    string Name, 
+    string LastName, 
+    string EmailAddress, 
+    string AddressLine, 
+    string City, 
+    string State, 
+    string ZipCode,
+    string Country
+);
+
+public record PaymentModel(
+    string CardName, 
+    string CardNumber, 
+    string Expiration, 
+    string Cvv, 
+    int PaymentMethod
+);
+
+public enum OrderStatus
+{
+    Draft = 1,
+    Pending = 2,
+    Completed = 3,
+    Cancelled = 4
+}
+
+//wrapper classes
+public record GetOrdersResponse(PaginatedResult<OrderModel> Orders);
+public record GetOrdersByNameResponse(IEnumerable<OrderModel> Orders);
+public record GetOrdersByCustomerResponse(IEnumerable<OrderModel> Orders);
