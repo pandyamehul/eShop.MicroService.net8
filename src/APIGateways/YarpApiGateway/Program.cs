90b35d5cd662b00106bc37af1f7360f024454d23
pandyamehul <pandya.mehul@live.com>
2025-07-12 16:37:48 +0200

    20250712#313: Rate Limiting to Yarp Reverse Proxy as an API Gateway in YarpApiGateway

--- src/APIGateways/YarpApiGateway/Program.cs
+++ src/APIGateways/YarpApiGateway/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.RateLimiting;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add application services to container
@@ -5,9 +7,20 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
+builder.Services.AddRateLimiter(rateLimiterOptions =>
+{
+    rateLimiterOptions.AddFixedWindowLimiter("fixed", options =>
+    {
+        options.Window = TimeSpan.FromSeconds(10);
+        options.PermitLimit = 5;
+    });
+});
+
 var app = builder.Build();
 
 // configuration http request pipeline
+app.UseRateLimiter();
+
 app.MapReverseProxy();
 
 app.Run();
